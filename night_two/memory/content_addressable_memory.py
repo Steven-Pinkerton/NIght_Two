d69fcb187ebebe78ec214defbbf743cfda4f941a
Steven <steven.a.pinkerton@gmail.com.com>
2023-07-24 02:22:21 +0100

    Reverted change, accidently pasted in tests our content addressable memory code. Reverted, still fighting errors on tests

--- night_two/memory/content_addressable_memory.py
+++ night_two/memory/content_addressable_memory.py
@@ -1,3 +1,4 @@
+import time
 import numpy as np
 from night_two.memory.memory_unit import MemoryUnit
 
@@ -7,34 +8,40 @@ class ContentAddressableMemoryUnit(MemoryUnit):
         super().__init__()
         self.N = N  # number of memory cells
         self.W = W  # size of memory vectors
-        # Initialize memory as a matrix of zeros with shape (N, W)
-        self.memory = np.zeros((N, W))
-        self.keys = np.zeros((N, W))  # Store keys separately
-        self.counter = 0  # Counter for the next free cell
+        self.memory = np.zeros((N, W))  
+        self.keys = np.zeros((N, W))
+        self.usage = np.full(N, np.inf)  # Unused cells have usage set to infinity
+        self.timestamp = np.zeros(N)  # New attribute to store timestamps
 
     def write(self, key: np.ndarray, data: np.ndarray):
         assert key.shape == (self.W,), "Key must be a 1D numpy array of size W"
         assert data.shape == (self.W,), "Data must be a 1D numpy array of size W"
-        
-        # Use a free cell or overwrite the oldest one
-        idx = self.counter % self.N
 
-        # Update the corresponding memory cell
+        # Find the index with the exact same key or the least recently used one
+        match_idx = np.where(np.all(self.keys == key, axis=1))
+        if match_idx[0].size > 0:
+            idx = match_idx[0][0]
+        else:
+            idx = np.argmin(self.timestamp)
+
+        self.keys[idx] = key
         self.memory[idx] = data
-        self.keys[idx] = key  # Save the key
-
-        print(f"Wrote data {data} with key {key} at index {idx}")
-
-        self.counter += 1
+        self.usage[idx] = 0
+        self.timestamp[idx] = time.time()  # Update the timestamp
+        self.usage += 1
 
     def read(self, key: np.ndarray) -> np.ndarray:
         assert key.shape == (self.W,), "Key must be a 1D numpy array of size W"
-        
-        # Find the key with the maximum similarity to the input key
-        similarities = np.dot(self.keys, key) / np.sqrt(self.W)
-        idx = np.argmax(similarities)
 
-        # Return the corresponding memory cell
-        read_data = self.memory[idx]
-        print(f"Read data {read_data} with key {self.keys[idx]} at index {idx}")
-        return read_data
+        # Find the index with the exact same key or the most recently used one
+        match_idx = np.where(np.all(self.keys == key, axis=1))
+        if match_idx[0].size > 0:
+            idx = match_idx[0][0]
+        else:
+            idx = np.argmax(self.timestamp)
+
+        self.usage[idx] = 0
+        self.timestamp[idx] = time.time()  # Update the timestamp
+        self.usage += 1
+
+        return self.memory[idx]
